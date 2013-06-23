4ae9e699c69612ed87545db9532f0f0c59d44f34
Daniel DOUAT <daniel.douat@aelys-info.fr>
2013-06-23 12:29:53 +0200

    mv form.php loginform.php

---
+++ loginform.php
@@ -0,0 +1,5 @@
+<form name="login" method="post" <?php echo $GLOBALS['loginAction'] ?>>
+    <input type="text" name="login"><br>
+    <input type="password" name="pass"><br>
+    <input type="submit" value="se connecter">
+</form>
